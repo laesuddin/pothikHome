52e0d58a9c2e7d15199f727a34c0ad12c433039d
Laes Uddin <laesuddin@gmail.com>
2023-02-13 19:29:12 +0600

    Create structure and layout

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Blog } from './blog/Blog'
+export { default as Footer } from './footer/Footer'
+export { default as Header } from './header/Header'
+export { default as Hero } from './hero/Hero'
+export { default as Packages } from './packages/Packages'
