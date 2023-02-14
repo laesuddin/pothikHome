8fd34985e6b0e26f66a56f8915e9fc14609b659e
Laes Uddin <laesuddin@gmail.com>
2023-02-14 19:35:43 +0600

    Fix popular tour section

--- src/components/packages/Packages.jsx
+++ src/components/packages/Packages.jsx
@@ -5,7 +5,7 @@ import './packages.css'
 
 const Packages = () => {
   return (
-    <section className="my-24 px-24 bg-[#F5FFFC]">
+  <section className="my-24 px-24 bg-[#F5FFFC]">
     <div className="text-centers mb-8">
       <h1
         className="title text-[#2ED89B] text-center text-4xl font-semibold uppercase"
@@ -18,140 +18,139 @@ const Packages = () => {
     </div>
     <div className="flex gap-x-8">
       <div className="w-1/3 shadow-lg">
-        <div className="mb-4">
-          <img
-            className="w-full"
-            src={img1}
-            alt="bandorban"
-          />
-        </div>
-        <div>
-          <div className="flex justify-between mx-3">
-            <h2 className="text-[#2ED89B] text-2xl font-semibold">BANDORBAN</h2>
-            <p><strong className="text-[#19A361] text-2xl">2500tk</strong></p>
+          <div className="mb-4">
+            <img
+              className="w-full"
+              src={img1}
+              alt="SYLHET"
+            />
           </div>
-          <div className="my-4 flex text-[#2D5527] gap-x-4 mx-1">
-            <div className="flex flex-col justify-center items-center">
-              <i className="fa-solid fa-user text-xl"></i>
-              <p className="flex flex-col text-xs ml-2">Single</p>
+          <div>
+            <div className="flex justify-between mx-3">
+              <h2 className="text-[#2ED89B] text-2xl font-semibold">SYLHET</h2>
+              <p><strong className="text-[#19A361] text-2xl">1500tk</strong></p>
             </div>
-            <div className="flex flex-col justify-center items-center">
-              <i className="fa-solid fa-user-group text-xl"></i>
-              <li className="flex flex-col text-xs ml-2">Couple</li>
+            <div className="my-4 flex text-[#2D5527] gap-x-4 mx-1">
+              <div className="flex flex-col justify-center items-center">
+                <i className="fa-solid fa-user text-xl"></i>
+                <p className="flex flex-col text-xs ml-2">Single</p>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <i className="fa-solid fa-user-group text-xl"></i>
+                <li className="flex flex-col text-xs ml-2">Couple</li>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <i className="fa-solid fa-users text-xl"></i>
+                <li className="flex flex-col text-xs ml-2">Family</li>
+              </div>
             </div>
-            <div className="flex flex-col justify-center items-center">
-              <i className="fa-solid fa-users text-xl"></i>
-              <li className="flex flex-col text-xs ml-2">Family</li>
-            </div>
-          </div>
-          <p className="w-10/12 text-[#6F6F6F] text-xs mx-3">
-            Lorem ipsum dolor sit amet This is Photoshop’s version of Lorem
-            Ipsn gravida. Ing business like this takes much more effort than
-            doing your own.
-          </p>
-          <div className="my-4 mx-3">
-            <p className="text-[#2D5527]">
-              <i className="fa-solid fa-calendar-check"></i><b>&nbsp; 3D 2N</b>
+            <p className="w-10/12 text-[#6F6F6F] text-xs mx-3">
+              Lorem ipsum dolor sit amet This is Photoshop’s version of Lorem
+              Ipsn gravida. Ing business like this takes much more effort than
+              doing your own.
             </p>
+            <div className="my-4 mx-3">
+              <p className="text-[#2D5527]">
+                <i className="fa-solid fa-calendar-check"></i><b>&nbsp; 2D 1N</b>
+              </p>
+            </div>
           </div>
+          <button className="w-full bg-[#FFCE0C] text-slate-100 py-2 mt-6">
+            <a className="text-xl" href="#">Book Your Tour</a>
+          </button>
         </div>
-        <button className="w-full bg-[#FFCE0C] text-slate-100 py-2 mt-6">
-          <a className="text-xl" href="#">Book Your Tour</a>
-        </button>
+
+
+        <div className="w-1/3 shadow-lg">
+          <div className="mb-4">
+            <img
+              className="w-full"
+              src={img2}
+              alt="SYLHET"
+            />
+          </div>
+          <div>
+            <div className="flex justify-between mx-3">
+              <h2 className="text-[#2ED89B] text-2xl font-semibold">SYLHET</h2>
+              <p><strong className="text-[#19A361] text-2xl">1500tk</strong></p>
+            </div>
+            <div className="my-4 flex text-[#2D5527] gap-x-4 mx-1">
+              <div className="flex flex-col justify-center items-center">
+                <i className="fa-solid fa-user text-xl"></i>
+                <p className="flex flex-col text-xs ml-2">Single</p>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <i className="fa-solid fa-user-group text-xl"></i>
+                <li className="flex flex-col text-xs ml-2">Couple</li>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <i className="fa-solid fa-users text-xl"></i>
+                <li className="flex flex-col text-xs ml-2">Family</li>
+              </div>
+            </div>
+            <p className="w-10/12 text-[#6F6F6F] text-xs mx-3">
+              Lorem ipsum dolor sit amet This is Photoshop’s version of Lorem
+              Ipsn gravida. Ing business like this takes much more effort than
+              doing your own.
+            </p>
+            <div className="my-4 mx-3">
+              <p className="text-[#2D5527]">
+                <i className="fa-solid fa-calendar-check"></i><b>&nbsp; 2D 1N</b>
+              </p>
+            </div>
+          </div>
+          <button className="w-full bg-[#FFCE0C] text-slate-100 py-2 mt-[21.10px]">
+            <a className="text-xl" href="#">Book Your Tour</a>
+          </button>
+        </div>
+
+        <div className="w-1/3 shadow-lg">
+          <div className="mb-4">
+            <img
+              className="w-full"
+              src={img3}
+              alt="SYLHET"
+            />
+          </div>
+          <div>
+            <div className="flex justify-between mx-3">
+              <h2 className="text-[#2ED89B] text-2xl font-semibold">SYLHET</h2>
+              <p><strong className="text-[#19A361] text-2xl">1500tk</strong></p>
+            </div>
+            <div className="my-4 flex text-[#2D5527] gap-x-4 mx-1">
+              <div className="flex flex-col justify-center items-center">
+                <i className="fa-solid fa-user text-xl"></i>
+                <p className="flex flex-col text-xs ml-2">Single</p>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <i className="fa-solid fa-user-group text-xl"></i>
+                <li className="flex flex-col text-xs ml-2">Couple</li>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <i className="fa-solid fa-users text-xl"></i>
+                <li className="flex flex-col text-xs ml-2">Family</li>
+              </div>
+            </div>
+            <p className="w-10/12 text-[#6F6F6F] text-xs mx-3">
+              Lorem ipsum dolor sit amet This is Photoshop’s version of Lorem
+              Ipsn gravida. Ing business like this takes much more effort than
+              doing your own.
+            </p>
+            <div className="my-4 mx-3">
+              <p className="text-[#2D5527]">
+                <i className="fa-solid fa-calendar-check"></i><b>&nbsp; 2D 1N</b>
+              </p>
+            </div>
+          </div>
+          <button className="w-full bg-[#FFCE0C] text-slate-100 py-2 mt-6">
+            <a className="text-xl" href="#">Book Your Tour</a>
+          </button>
+        </div>
       </div>
 
-      <div className="w-1/3 shadow-lg">
-        <div className="mb-4">
-          <img
-            className="w-full"
-            src={img2}
-            alt="SYLHET"
-          />
-        </div>
-        <div>
-          <div className="flex justify-between mx-3">
-            <h2 className="text-[#2ED89B] text-2xl font-semibold">SYLHET</h2>
-            <p><strong className="text-[#19A361] text-2xl">1500tk</strong></p>
-          </div>
-          <div className="my-4 flex text-[#2D5527] gap-x-4 mx-1">
-            <div className="flex flex-col justify-center items-center">
-              <i className="fa-solid fa-user text-xl"></i>
-              <p className="flex flex-col text-xs ml-2">Single</p>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <i className="fa-solid fa-user-group text-xl"></i>
-              <li className="flex flex-col text-xs ml-2">Couple</li>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <i className="fa-solid fa-users text-xl"></i>
-              <li className="flex flex-col text-xs ml-2">Family</li>
-            </div>
-          </div>
-          <p className="w-10/12 text-[#6F6F6F] text-xs mx-3">
-            Lorem ipsum dolor sit amet This is Photoshop’s version of Lorem
-            Ipsn gravida. Ing business like this takes much more effort than
-            doing your own.
-          </p>
-          <div className="my-4 mx-3">
-            <p className="text-[#2D5527]">
-              <i className="fa-solid fa-calendar-check"></i><b>&nbsp; 2D 1N</b>
-            </p>
-          </div>
-        </div>
-        <button className="w-full bg-[#FFCE0C] text-slate-100 py-2 mt-6">
-          <a className="text-xl" href="#">Book Your Tour</a>
-        </button>
+      <div className="mt-12 flex justify-center items-center">
+        <a className="bg-[#FFCE0C] text-slate-100 px-6 py-2 rounded" href="#">More</a>
       </div>
-
-      <div className="w-1/3 shadow-lg">
-        <div className="mb-4">
-          <img
-            className="w-full"
-            src={img3}
-            alt="KUAKATA"
-          />
-        </div>
-        <div>
-          <div className="flex justify-between mx-3">
-            <h2 className="text-[#2ED89B] text-2xl font-semibold">KUAKATA</h2>
-            <p><strong className="text-[#19A361] text-2xl">2500tk</strong></p>
-          </div>
-          <div className="my-4 flex text-[#2D5527] gap-x-4 mx-1">
-            <div className="flex flex-col justify-center items-center">
-              <i className="fa-solid fa-user text-xl"></i>
-              <p className="flex flex-col text-xs ml-2">Single</p>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <i className="fa-solid fa-user-group text-xl"></i>
-              <li className="flex flex-col text-xs ml-2">Couple</li>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <i className="fa-solid fa-users text-xl"></i>
-              <li className="flex flex-col text-xs ml-2">Family</li>
-            </div>
-          </div>
-          <p className="w-10/12 text-[#6F6F6F] text-xs mx-3">
-            Lorem ipsum dolor sit amet This is Photoshop’s version of Lorem
-            Ipsn gravida. Ing business like this takes much more effort than
-            doing your own.
-          </p>
-          <div className="my-4 mx-3">
-            <p className="text-[#2D5527]">
-              <i className="fa-solid fa-calendar-check"></i><b>&nbsp; 4D 3N</b>
-            </p>
-          </div>
-        </div>
-        <button className="w-full bg-[#FFCE0C] text-slate-100 py-2 mt-6">
-          <a className="text-xl" href="#">Book Your Tour</a>
-        </button>
-      </div>
-    </div>
-
-    <div className="mt-12 flex justify-center items-center">
-      <a className="bg-[#FFCE0C] text-slate-100 px-6 py-2 rounded" href="#"
-        >More</a
-      >
-    </div>
   </section>
   )
 } 
